7bc4e82985fa496fe60486bc2febcae5fd303900
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-25 09:33:47 -0400

    Render all stocks

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,22 +1,31 @@
-import React from "react";
-import StockContainer from "./StockContainer";
-import PortfolioContainer from "./PortfolioContainer";
-import SearchBar from "./SearchBar";
+import React, { useEffect, useState } from "react"
+import StockContainer from "./StockContainer"
+import PortfolioContainer from "./PortfolioContainer"
+import SearchBar from "./SearchBar"
 
 function MainContainer() {
+  const [fetchTrigger, setFetchTrigger] = useState(false)
+  const [stocks, setStocks] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((data) => setStocks(data))
+  }, [fetchTrigger])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default MainContainer;
+export default MainContainer
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React from "react"
 
-function Stock() {
+function Stock({ stock: { ticker, name, type, price } }) {
   return (
     <div>
-      <div className="card">
+      <div className="card" value={type}>
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">
+            {ticker}: {price}
+          </p>
         </div>
       </div>
     </div>
-  );
+  )
 }
-export default Stock;
+export default Stock
